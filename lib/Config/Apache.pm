c16e75db08538a1892e69fa85695cb95e25cd5ab
Joshua Yotty <joshyotty@gmail.com>
2009-11-20 20:16:22 -0800

    definedness not truth

--- lib/Config/Apache.pm
+++ lib/Config/Apache.pm
@@ -112,7 +112,7 @@ sub BUILD {
     # ripped the double quoted matcher from perlre.
     # I'll be honest, I have no idea where the undefs come from.
     if ($self->value =~ /\s|"/) {
-        my @args = grep { $_ } $self->value =~ /"((?>(?:(?>[^"\\]+)|\\.)*))"|(\S+)/g;
+        my @args = grep { defined } $self->value =~ /"((?>(?:(?>[^"\\]+)|\\.)*))"|(\S+)/g;
         $self->value(@args > 1 ? \@args : $args[0]);
     }
 }
